d2af8ab6fd53b59080f5a5569b0c97ea939675ec
Liebault <dan.liebault@viacesi.fr>
2019-05-04 16:24:47 +0200

    move screen handle to asm

--- drivers/screen.h
+++ drivers/screen.h
@@ -11,9 +11,8 @@
 #define WHITE_ON_BLACK 0x0f
 
 //screen I/O ports
-#define REG_SCREEN_CTRL 0x3D4
-#define REG_SCREEN_DATA 0x3D5
+#define REG_SCREEN_CTRL 0x3D4//type of data
+#define REG_SCREEN_DATA 0x3D5//data itself
 
-void print_char(char c, int col, int row, char attrib_byte);
 
 #endif // !_SCREEN_H
