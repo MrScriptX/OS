30c9918c4e65227e3610477352c283e1c99e83d7
LIEBAULT <dan.liebault@sogeti.com>
2020-01-10 13:46:16 +0100

    fix: multiple inclusion of func

--- drivers/low_level.h
+++ drivers/low_level.h
@@ -2,26 +2,26 @@
 #define _LOW_LEVEL_H
 
 
-unsigned char port_byte_in(unsigned short port)
+static inline unsigned char port_byte_in(unsigned short port)
 {
 	unsigned short result;
 	__asm__("in %%dx, %%al" : "=a"(result) : "d"(port));
 	return result;
 }
 
-void port_byte_out(unsigned short port, unsigned char data)
+static inline void port_byte_out(unsigned short port, unsigned char data)
 {
 	__asm__("out %%al, %%dx" : : "a"(data), "d"(port));
 }
 
-unsigned short port_word_in(unsigned short port)
+static inline unsigned short port_word_in(unsigned short port)
 {
 	unsigned short result;
 	__asm__("in %%dx, %%ax" : "=a"(result) : "d"(port));
 	return result;
 }
 
-void port_word_out(unsigned short port, unsigned short data)
+static inline void port_word_out(unsigned short port, unsigned short data)
 {
 	__asm__("out %%ax, %%dx" : : "a"(data), "d"(port));
 }
