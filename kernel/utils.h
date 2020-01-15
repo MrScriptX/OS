3e851c0e364fcdb8a1e1a363f83e5ac4aab98838
Dan Liebault <dan.liebault@viacesi.fr>
2020-01-15 16:06:17 +0100

    #5 keyboard print text

--- kernel/utils.h
+++ kernel/utils.h
@@ -2,7 +2,9 @@
 #define _UTILS_H
 
 void int_to_ascii(int n, char str[]);
+void hex_to_ascii(int n, char str[]);
 void reverse(char s[]);
 int strlen(char s[]);
+void append(char s[], char n);
 
 #endif
