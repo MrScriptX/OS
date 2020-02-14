a56126edce10730a2097f7c556b7aac53c83e912
Dan Liebault <dan.liebault@viacesi.fr>
2020-02-14 15:31:31 +0100

    try to enable paging

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -1,9 +1,19 @@
+[extern __main]
+
 global _start
 
 [bits 32]
 section .text.bootstrap
 
 _start:
-[extern __main]
+
+; enable paging
+;mov eax, PAGE_DIRECTORY
+;mov cr3, eax
+
+mov cr0, eax
+or  eax, 0x00001000
+mov eax, cr0
+
 call __main
 jmp $
